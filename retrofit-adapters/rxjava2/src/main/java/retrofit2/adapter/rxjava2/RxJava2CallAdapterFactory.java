9ac26aae8ba8ac9d6fec27331a622106f0bb0b8b
FaridFarahmandf <f_fari@live.concordia.ca>
2025-11-01 20:18:36 -0400

    changed the nested ternary operation to if statements

--- retrofit-adapters/rxjava2/src/main/java/retrofit2/adapter/rxjava2/RxJava2CallAdapterFactory.java
+++ retrofit-adapters/rxjava2/src/main/java/retrofit2/adapter/rxjava2/RxJava2CallAdapterFactory.java
@@ -113,8 +113,19 @@ public final class RxJava2CallAdapterFactory extends CallAdapter.Factory {
     boolean isBody = false;
     Type responseType;
     if (!(returnType instanceof ParameterizedType)) {
-      String name =
-          isFlowable ? "Flowable" : isSingle ? "Single" : isMaybe ? "Maybe" : "Observable";
+
+      String name;
+      if (isFlowable) {
+        name = "Flowable";
+      } else if (isSingle) {
+        name = "Single";
+      } else if (isMaybe) {
+        name = "Maybe";
+      } else {
+        name = "Observable";
+      }
+
+          
       throw new IllegalStateException(
           name
               + " return type must be parameterized"
